c2c6ec021274670de7b7e58fcce08a0e5656bf92
logic <1421499510@qq.com>
2023-11-06 08:16:00 +0800

    复习多线程

--- October/src/main/Demo12.java
+++ October/src/main/Demo12.java
@@ -2,7 +2,7 @@ package src.main;
 
 /**
  * 1. 线程等待,线程之间的调度的顺序是不确定的
- * 我们可以通过一些特殊的操作,来对现场的执行顺序人为进行干预,join()方法就是其中的一个方式,控制线程之间的结束顺序
+ * 我们可以通过一些特殊的操作,来对线程的执行顺序人为进行干预,join()方法就是其中的一个方式,控制线程之间的结束顺序
  * 在main线程中使用thread.join()就是让main线程阻塞等待等到thread执行完了,然后main才会继续执行,这个时候线程
  *
  * 2.调用的原则:
@@ -13,6 +13,7 @@ public class Demo12 {
     public static void main(String[] args){
         //创建线程
         Thread thread  = new Thread(()->{
+            System.out.println("thread1线程开始执行!");
          for (int i = 0;i < 5;i++){
                System.out.println("hello thread !");
                try {
--- October/src/main/Demo13.java
+++ October/src/main/Demo13.java
@@ -1,8 +1,39 @@
 package src.main;
 
+/**
+ * 获取当前线程的引用 currentThread() 返回当前线程对象的引用
+ */
 public class Demo13 {
     public static void main(String[] args){
-    String s = new String();
+
+        Thread t1 = new Thread(()->{
+            while(!Thread.currentThread().isInterrupted()){
+                System.out.println("thread1");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+            System.out.println("thread线程执行完了!");
+        });
+
+        t1.start();
+        System.out.println("main线程开始!");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("mian线程执行完了!");
+
+        /*
+        我们可以在main方法中随意定义一个逻辑用来阻塞作为thread1 线程终止的一个判断条件,我们这里在main方法中实现的逻辑就是
+        等main执行3秒之后就可以把thread线程给结束掉了,所以这个时候我们就需要把系统调用的方法给修改成阻塞状态
+         */
+        t1.interrupt();
+        System.out.println("设置让mian线程结束!");
     }
 
 }
