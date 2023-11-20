7332657e9b9b1de701f735f66270d60c52b01519
logic <1421499510@qq.com>
2023-11-20 08:40:19 +0800

    线索二叉树测试

---
+++ October/src/test/java/test.java
@@ -0,0 +1,13 @@
+package src.test.java;
+
+public class test {
+    static{
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args){
+        System.out.println("mian方法");
+        String s = new String("abc");
+        final char[] arr= {'a','b','c'};
+        arr[0] = '1';
+    }
+}
